082ab99c2c36d984a546e781a4947171ba1a2bea
ddk-0310 <ddk-0310@yandex.ru>
2019-09-29 23:09:03 +0300

    Some changes

--- TrackProject/ViewController.swift
+++ TrackProject/ViewController.swift
@@ -11,8 +11,33 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        print("Func: viewDidLoad")
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("Func: viewDidAppear")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("Func: viewWillAppear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+           print("Func: viewDidDisappear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("Func: viewWillDissappear")
+    }
+    
+    override func viewDidLayoutSubviews() {
+           print("Func: viewDidLayoutSubviews")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        print("Func: viewWillLayoutSubviews")
     }
 
 
