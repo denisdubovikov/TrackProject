5ed04229d2fc8f82a203d79d4ecc3222376eb6b7
ddk-0310 <ddk-0310@yandex.ru>
2019-09-30 00:04:53 +0300

    Initial Commit

--- TrackProject/AppDelegate.swift
+++ TrackProject/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        print("Func: didFinishLaunchingWithOptions")
+        print("Func: didFinishLaunchingWithOptions. Tells the delegate that the launch process is almost done and the app is almost ready to run.")
         return true
     }
 
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
-        print("Func: configurationForConnecting")
+        print("Func: configurationForConnecting. Returns the configuration data for UIKit to use when creating a new scene.")
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
@@ -32,7 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-        print("Func: didDiscardSceneSessions")
+        print("Func: didDiscardSceneSessions. Tells the delegate that the user closed one or more of the app's scenes from the app switcher.")
     }
 
 
--- TrackProject/ViewController.swift
+++ TrackProject/ViewController.swift
@@ -11,33 +11,33 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        print("Func: viewDidLoad")
+        print("Func: viewDidLoad. Called after the controller's view is loaded into memory.")
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        print("Func: viewDidAppear")
+        print("Func: viewDidAppear. Notifies the view controller that its view was added to a view hierarchy.")
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        print("Func: viewWillAppear")
+        print("Func: viewWillAppear. otifies the view controller that its view is about to be added to a view hierarchy.")
     }
     
     override func viewDidDisappear(_ animated: Bool) {
-           print("Func: viewDidDisappear")
+           print("Func: viewDidDisappear. Notifies the view controller that its view was removed from a view hierarchy.")
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        print("Func: viewWillDissappear")
+        print("Func: viewWillDissappear. Notifies the view controller that its view is about to be removed from a view hierarchy.")
     }
     
     override func viewDidLayoutSubviews() {
-           print("Func: viewDidLayoutSubviews")
+           print("Func: viewDidLayoutSubviews. Called to notify the view controller that its view has just laid out its subviews.")
     }
     
     override func viewWillLayoutSubviews() {
-        print("Func: viewWillLayoutSubviews")
+        print("Func: viewWillLayoutSubviews. Called to notify the view controller that its view is about to layout its subviews.")
     }
 
 
