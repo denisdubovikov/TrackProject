dc11310c74f55fd7fa46f7409e98573c41b3ea3e
ddk-0310 <ddk-0310@yandex.ru>
2019-09-30 15:14:53 +0300

    Initial Commit

--- TrackProject/AppDelegate.swift
+++ TrackProject/AppDelegate.swift
@@ -11,29 +11,53 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         print("Func: didFinishLaunchingWithOptions. Tells the delegate that the launch process is almost done and the app is almost ready to run.")
         return true
     }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        print("Func: applicationDidBecomeActive. Tells the delegate that the app has become active.")
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        print("Func: applicationWillResignActive. Tells the delegate that the app is about to become inactive.")
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        print("Func: applicationDidEnterBackground. Tells the delegate that the app is now in the background.")
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        print("Func: applicationWillEnterForeground. Tells the delegate that the app is about to enter the foreground.")
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        print("Func: applicationWillTerminate. Tells the delegate when the app is about to terminate.")
+    }
+    
+    
+    
+    
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        print("Func: configurationForConnecting. Returns the configuration data for UIKit to use when creating a new scene.")
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-        print("Func: didDiscardSceneSessions. Tells the delegate that the user closed one or more of the app's scenes from the app switcher.")
-    }
+//    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+//        // Called when a new scene session is being created.
+//        // Use this method to select a configuration to create the new scene with.
+//        print("Func: configurationForConnecting. Returns the configuration data for UIKit to use when creating a new scene.")
+//        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+//    }
+//
+//    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+//        // Called when the user discards a scene session.
+//        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+//        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+//        print("Func: didDiscardSceneSessions. Tells the delegate that the user closed one or more of the app's scenes from the app switcher.")
+//    }
 
 
 }
